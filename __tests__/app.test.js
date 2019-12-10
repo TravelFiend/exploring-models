a6b88ecebfbac460ffb8117496a6c2f609384621
TravelFiend <mikegrace274@gmail.com>
2019-12-09 21:40:01 -0800

    POST test written and passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,6 @@ const connect = require('../lib/utils/connect');
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../lib/app');
-
 const Beer = require('../lib/models/Beer');
 
 describe('app routes', () => {
@@ -18,4 +17,33 @@ describe('app routes', () => {
     afterAll(() => {
         return mongoose.connection.close();
     });
+
+    it('creates a beer item on POST', () => {
+        return request(app)
+            .post('/beer')
+            .send({
+                'brand': 'miller',
+                'name': 'lite',
+                'brewType': 'lager',
+                'abv': 4.6,
+                'volume': '12 oz',
+                'agedYears': 0.5,
+                'bottle': false
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    brand: 'miller',
+                    name: 'lite',
+                    brewType: 'lager',
+                    abv: 4.6,
+                    volume: '12 oz',
+                    agedYears: 0.5,
+                    bottle: false,
+                    __v: 0
+                });
+            });
+    });
+
+    
 });
