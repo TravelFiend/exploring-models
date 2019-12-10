9bf6ebd808267c3a3ae1228f2460d70530efcc97
TravelFiend <mikegrace274@gmail.com>
2019-12-10 11:53:25 -0800

    travis is an a**

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
-// require('../lib/utils/connect')();
+const connect = require('../lib/utils/connect');
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../lib/app');
 const Beer = require('../lib/models/Beer');
 
 describe('app routes', () => {
+    beforeAll(() => {
+        connect();
+    });
 
     beforeEach(() => {
         return mongoose.connection.dropDatabase();
